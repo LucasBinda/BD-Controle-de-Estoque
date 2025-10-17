9295d3da225a3dd7cd94d52d8ab7123c06547ab6
Lucas Binda <137837485+LucasBinda@users.noreply.github.com>
2025-10-17 14:44:09 -0300

    Deleted

--- Python/connection/oracle_query.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-from typing import Dict, Optional
-from database import DatabaseConnection
-
-class OracleQuery:
-    def __init__(self):
-        self.db = DatabaseConnection()
-
